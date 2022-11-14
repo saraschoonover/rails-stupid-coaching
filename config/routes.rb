f67867409f52fa9b4f107c0fb4a6ff6b5370ae58
Sara Schoonover <saramschoon@gmail.com>
2022-11-14 14:53:33 +0000

    completed stupid coaching exercise

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # VERB 'path', to: 'controller#action'
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
